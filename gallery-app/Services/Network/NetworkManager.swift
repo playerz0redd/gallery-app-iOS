872941b024b4007955d7e1d76fdf1f4afb23f5b2
playerz0redd <ipasha1337@yandex.by>
2025-12-09 01:38:29 +0300

    [ADDED] Added network manager

--- gallery-app/Services/Network/NetworkManager.swift
+++ gallery-app/Services/Network/NetworkManager.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+final class NetworkManager {
+    
+    func fetchData(endpoint: APIEndpoints) async throws -> Data {
+        let url = URL(string: endpoint.stringValue)!
+        let request = URLRequest(url: url)
+        let (data, response) = try await URLSession.shared.data(for: request)
+        return data
+    }
+}
