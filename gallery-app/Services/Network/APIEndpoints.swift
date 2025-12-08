e98d81785376dcb6aae259bb62d3bc97f3473bf2
playerz0redd <ipasha1337@yandex.by>
2025-12-09 01:29:40 +0300

    [ADDED] Added enum api endpoints

---
+++ gallery-app/Services/Network/APIEndpoints.swift
@@ -0,0 +1,24 @@
+//
+//  APIEndpoints.swift
+//  gallery-app
+//
+//  Created by Pavel Playerz0redd on 9.12.25.
+//
+
+import Foundation
+
+enum APIEndpoints {
+    case imageInfo(page: Int)
+    case downloadImage(url: String)
+    
+    static let accessToken: String = "9v4f0Itk2mWXa831ATj5ZbIxu1FIyL6Xw0vpv4wJ7mM"
+    
+    var stringValue: String {
+        switch self {
+        case .imageInfo(let page):
+            "https://api.unsplash.com/photos/?client_id=\(Self.accessToken)&page=\(page)"
+        case .downloadImage(let url):
+            url
+        }
+    }
+}
---
+++ gallery-app/Services/Network/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkManager.swift
+//  gallery-app
+//
+//  Created by Pavel Playerz0redd on 9.12.25.
+//
+
+import Foundation
