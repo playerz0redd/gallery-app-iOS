1719d87e2dc96c20b81f37fd7e2e69a26e95eeac
playerz0redd <ipasha1337@yandex.by>
2025-12-09 00:52:44 +0300

    Initial commit

---
+++ gallery-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gallery-app
+//
+//  Created by Pavel Playerz0redd on 9.12.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
