d04a1931c4997ddfc31150407e1dfd27f06a9a32
playerz0redd <ipasha1337@yandex.by>
2025-12-16 01:51:33 +0300

    [ADDED] Added gallery module builder

---
+++ gallery-app/Modules/Gallery/Builder/GalleryBuilder.swift
@@ -0,0 +1,51 @@
+//
+//  GalleryBuilder.swift
+//  gallery-app
+//
+//  Created by Pavel Playerz0redd on 16.12.25.
+//
+
+import Foundation
+import UIKit
+
+final class GalleryModuleBuilder {
+    
+    enum GalleryModuleType {
+        case gallery
+        case favorites
+        
+        var title: String {
+            switch self {
+            case .gallery:       "Gallery"
+            case .favorites:     "Favorites"
+            }
+        }
+        
+        var imageName: String {
+            switch self {
+            case .gallery:       "photo.on.rectangle"
+            case .favorites:     "heart.fill"
+            }
+        }
+    }
+    
+    static func build(
+        dataProvider: NetworkManager,
+        persistanceProvider: CoreDataManager,
+        type: GalleryModuleType
+    ) -> GalleryViewController {
+        
+        let modelProvider: IModelProvider = type == .favorites ? persistanceProvider : dataProvider
+        let photoService: PhotoService = .init(
+            dataProvider: dataProvider,
+            modelProvider: modelProvider,
+            persistanceManager: persistanceProvider
+        )
+        let viewModel = GalleryViewModel(photoService: photoService)
+        let viewController = GalleryViewController(viewModel: viewModel)
+        viewController.navigationItem.title = type.title
+        
+        return viewController
+    }
+    
+}
