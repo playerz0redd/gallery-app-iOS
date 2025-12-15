ca22011802da46e5d20b49ee4d0548dfec5db6f5
playerz0redd <ipasha1337@yandex.by>
2025-12-15 18:24:45 +0300

    [REFACTOR] Added gallery cell collection configurating methods

--- gallery-app/Modules/Gallery/View/GalleryCell.swift
+++ gallery-app/Modules/Gallery/View/GalleryCell.swift
@@ -35,4 +35,39 @@ class GalleryCell: UICollectionViewCell {
         self.imageView.image = nil
     }
     
+    func setupImageView() -> UIImageView {
+        let image = UIImageView()
+        image.contentMode = .scaleAspectFill
+        image.layer.cornerRadius = 10
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.clipsToBounds = true
+        return image
+    }
+    
+    func configureCell(with url: String, photoService: PhotoService) {
+        self.imageUrl = url
+        self.photoService = photoService
+        Task { @MainActor in
+            let image = try await photoService.fetchPhoto(for: .downloadImage(url: url))
+            
+            if imageUrl == self.imageUrl {
+                self.imageView.image = image
+            }
+        }
+    
+    }
+    
+    func setupLayout() {
+        contentView.addSubview(imageView)
+        contentView.layer.cornerRadius = 10
+        contentView.clipsToBounds = true
+        
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            imageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            imageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor)
+        ])
+    }
+    
 }
