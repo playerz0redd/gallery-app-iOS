d6d390f718e357a80ddf568c2c990567c6098f6e
playerz0redd <ipasha1337@yandex.by>
2025-12-15 18:40:58 +0300

    [ADDED] Added photo model to core data as dto

---
+++ gallery-app/Managers/CoreDataManager/DatabasePhotoModel.swift
@@ -0,0 +1,17 @@
+//
+//  DatabasePhotoModel.swift
+//  gallery-app
+//
+//  Created by Pavel Playerz0redd on 15.12.25.
+//
+
+import Foundation
+
+struct DatabasePhotoModel {
+    let id: String
+    let description: String?
+    let likes: Int
+    let regularUrl: String
+    let thumbUrl: String
+    let username: String?
+}
