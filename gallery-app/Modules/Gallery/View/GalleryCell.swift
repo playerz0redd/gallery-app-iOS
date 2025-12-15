d11d8ef86c4fabc48d202972f9191612e74dd8b4
playerz0redd <ipasha1337@yandex.by>
2025-12-15 18:20:21 +0300

    [ADDED] Added gallery collection cell with reuse id

---
+++ gallery-app/Modules/Gallery/View/GalleryCell.swift
@@ -0,0 +1,16 @@
+//
+//  GalleryCell.swift
+//  gallery-app
+//
+//  Created by Pavel Playerz0redd on 10.12.25.
+//
+
+import UIKit
+
+
+class GalleryCell: UICollectionViewCell {
+    
+    class var reuseId: String {
+        "galleryCell"
+    }
+}
