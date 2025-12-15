b5c41c26139ab7febe0c6951c1cb9a55f0366dc2
playerz0redd <ipasha1337@yandex.by>
2025-12-15 18:23:20 +0300

    [REFACTOR] Added inits and view properties to gallery collection view cell

--- gallery-app/Modules/Gallery/View/GalleryCell.swift
+++ gallery-app/Modules/Gallery/View/GalleryCell.swift
@@ -13,4 +13,26 @@ class GalleryCell: UICollectionViewCell {
     class var reuseId: String {
         "galleryCell"
     }
+    
+    private var photoService: PhotoService?
+    private var imageUrl: String = ""
+    
+    lazy var imageView: UIImageView = {
+        setupImageView()
+    }()
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupLayout()
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        self.imageView.image = nil
+    }
+    
 }
