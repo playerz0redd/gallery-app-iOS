9865c673f0b287fffa372b986a075dc7e1fdb312
playerz0redd <ipasha1337@yandex.by>
2025-12-15 23:49:07 +0300

    [ADDED] Created details view controller with collection view

---
+++ gallery-app/Modules/GalleryPhotoDetails/View/GalleryPhotoDetailsViewController.swift
@@ -0,0 +1,53 @@
+//
+//  GalleryPhotoDetailsViewController.swift
+//  gallery-app
+//
+//  Created by Pavel Playerz0redd on 10.12.25.
+//
+
+import UIKit
+
+final class GalleryPhotoDetailsViewController: UIViewController {
+    
+    private let viewModel: GalleryPhotoDetailsViewModel
+    private var currentImageIndex: Int
+    private var isFirstLayout = true
+    
+    init(viewModel: GalleryPhotoDetailsViewModel, currentImageIndex: Int) {
+        self.viewModel = viewModel
+        self.currentImageIndex = currentImageIndex
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    lazy private var collectionView: UICollectionView = {
+        let layout: UICollectionViewFlowLayout = .init()
+        layout.minimumLineSpacing = 0
+        layout.scrollDirection = .horizontal
+        let collection: UICollectionView = .init(frame: .zero, collectionViewLayout: layout)
+        collection.dataSource = self
+        collection.delegate = self
+        collection.register(GalleryDetailsCell.self, forCellWithReuseIdentifier: GalleryDetailsCell.reuseId)
+        collection.translatesAutoresizingMaskIntoConstraints = false
+        collection.isPagingEnabled = true
+        collection.showsHorizontalScrollIndicator = false
+        return collection
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.title = "Details"
+        bindViewModel()
+        view.backgroundColor = .systemBackground
+        view.addSubview(collectionView)
+        
+    }
+    
+    
+}
+
+
+
