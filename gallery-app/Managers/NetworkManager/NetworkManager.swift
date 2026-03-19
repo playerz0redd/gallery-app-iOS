d18969e7f2c6ec95911307bd3f85f0b94264a0b7
pavel.bychkouski <ipasha1337@yandex.by>
2026-03-19 22:39:21 +0300

    [REFACTOR] Changed url configuration property name in network manager

--- gallery-app/Managers/NetworkManager/NetworkManager.swift
+++ gallery-app/Managers/NetworkManager/NetworkManager.swift
@@ -9,22 +9,24 @@ import Foundation
 
 struct NetworkManager: IModelProvider, IDataProvider {
     
+    private let successCode = 200
+    
     func fetchPhotosModels(page: Int) async throws(AppError) -> [ImageModel] {
         let data = try await self.fetchData(endpoint: APIEndpoints.imageInfo(page: page))
         return try Decoder.decode(type: [ImageModel].self, data: data)
     }
     
-    
     func fetchData(endpoint: APIEndpoints) async throws(AppError) -> Data {
-        guard let url = URL(string: endpoint.stringValue) else { throw .photoModelError }
+        guard let url = URL(string: endpoint.url) else { throw .photoModelError }
         var request = URLRequest(url: url)
         request.timeoutInterval = 5
         do {
             let (data, response) = try await URLSession.shared.data(for: request)
             
-            if let response = response as? HTTPURLResponse, response.statusCode != 200 {
+            if let response = response as? HTTPURLResponse, response.statusCode != successCode {
                 throw NetworkError.serverError(serverError: .statusCode(code: response.statusCode))
             }
+            
             return data
         } catch let error {
             throw .networkError(.internetError(error))
